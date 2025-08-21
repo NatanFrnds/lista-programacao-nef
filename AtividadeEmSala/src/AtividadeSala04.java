9ca5fe4ffe59fa06bd9768f357e30dd5200642d7
NatanFrnds <natanemanuelfernandes870@gmail.com>
2025-08-20 21:19:26 -0300

    Lista Atividade 4-6

---
+++ AtividadeEmSala/src/AtividadeSala04.java
@@ -0,0 +1,17 @@
+public class AtividadeSala04 {
+    public static void main(String[] args) {
+        int x = 0;
+        x += 1;
+        System.out.println("x += 1 = " + x);
+        x++;
+        System.out.println("x ++ = " + x);
+        x -= 2;
+        System.out.println("x -= 2 = " + x);
+        //Qual o valor final de x?
+        System.out.println("Valor de X atual é:" + x);
+        // Imprima o valor de x em cada operação
+        //Qual o resultado de x se
+        x--;
+        System.out.print("x-- = " + x);
+    }
+}
---
+++ AtividadeEmSala/src/Atividadesala05.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Atividadesala05 {
+    public static void main(String[] args) {
+        char letra = 'S';
+        char inicial = 'V';
+        String texto = "Code";
+        //Concatenação
+        String temp="";
+    temp="" +inicial+letra;
+    System.out.println(temp);
+    System.out.println(temp + texto);
+
+    }
+}
