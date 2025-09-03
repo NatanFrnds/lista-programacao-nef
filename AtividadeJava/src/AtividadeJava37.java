93ca896c1c0fa777c410535deeae479f7fc9b066
NatanFrnds <natanemanuelfernandes870@gmail.com>
2025-09-02 21:28:35 -0300

    36 a 46

---
+++ AtividadeJava/src/AtividadeJava37.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AtividadeJava37 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for (int n = 0; n <= 10; n++) {
+            System.out.println(n);
+        }
+    }
+}
